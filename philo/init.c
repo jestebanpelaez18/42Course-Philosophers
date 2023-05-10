836e54d7d412470a5c38753bb1386331b575cf3d
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-05-10 17:20:54 +0300

    vars init

--- philo/init.c
+++ philo/init.c
@@ -1,36 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/09 17:31:01 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/05/10 17:13:54 by jpelaez-         ###   ########.fr       */
+/*   Created: 2023/05/10 17:12:59 by jpelaez-          #+#    #+#             */
+/*   Updated: 2023/05/10 17:20:41 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "philo.h"
 
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <sys/time.h>
-# include <unistd.h>
-
-typedef struct s_list
+void	init_philo(t_list *philo, char **phi_arg)
 {
-	int	n_philo;
-	int	t_die;
-	int	t_sleep;
-	int	t_eat;
-	int	n_times_eat;
-}		t_list;
-
-int		ft_atoi(char *number);
-
-
-void	ft_putendl_fd(char *s, int fd);
-void	create_vars(t_list *philo, char **phi_arg);
-#endif
+	philo->n_philo = ft_atoi(phi_arg[1]);
+	philo->t_die = ft_atoi(phi_arg[2]);
+	philo->t_eat = ft_atoi(phi_arg[3]);
+	philo->t_sleep = ft_atoi(phi_arg[4]);
+}
--- philo/main.c
+++ philo/main.c
@@ -6,24 +6,21 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:14:30 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/05/10 16:52:44 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/05/10 17:13:31 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	create_vars(char **phi_arg)
-{
-	
-}
-
 int	main(int argc, char **argv)
 {
+	t_list	philo;
+
 	if (argc != 6 || argc != 5)
 	{
 		ft_putendl_fd("Error, number of arguments invalid", 2);
 		return (0);
 	}
-	create_vars(argv);
+	init_philo(&philo, argv);
 	return (0);
 }
